9486d6005f6ba6e787a9c43144e4e68e87b7cccf
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-27 19:13:02 +0200

    Create a traffic fortune service

--- Spring-AOP-Around/src/com/gk/aop/service/TrafficFortuneService.java
+++ Spring-AOP-Around/src/com/gk/aop/service/TrafficFortuneService.java
@@ -1,5 +1,19 @@
 package com.gk.aop.service;
 
+import java.util.concurrent.TimeUnit;
+
 public class TrafficFortuneService {
 
+	public String getFortune() {
+		// simulate a delay
+		try {
+
+			TimeUnit.SECONDS.sleep(5);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// return a fortune
+		return "Expect Heavy Traffic this morning";
+	}
 }
